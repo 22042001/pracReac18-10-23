7ac7eb6437f52466454707604feda97f850eae42
Teddy_2204 <anttoniogonj56@gmail.com>
2023-10-18 09:31:14 -0400

    miprimerComponente

---
+++ pagina1/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          <code>Hola/Teddy</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hola Mundo con React 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ pagina1/src/componentes/Header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css" // importamos el archivo del estilo del header
+
+function Header(){ //creamos una funcion Header con retorno y usamos la etiquetas html para colocar la imagen
+    return <header className="header"> 
+            <img src = "/img/HeaderHeader_total.png" alt="ghost"/>
+    </header>
+}
+
+export default Header //esportamos 
